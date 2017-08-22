ec37d1b0a79a189917611bc4f023b68b4efd024f
opensmarty <opensmarty@163.com>
2017-08-23 00:18:44 +0800

    构建基础的前端启动系统

--- src/public/static/script/core/system.js
+++ src/public/static/script/core/system.js
@@ -17,7 +17,7 @@ Engine.prototype = {
     constructor: Engine,
     name: '起源引擎',
     type: 'engine',
-    description: 'System Service',
+    description: 'Engine Service',
     execute: function () {
         this.beforeExecute();
         this.doExecute();
@@ -33,6 +33,9 @@ Engine.prototype = {
     afterExecute: function () {
         console.log('afterExe');
     },
+    extra: function () {
+
+    },
     doSomething: function () {
         return "This is a " + this.description + " for us to do something.";
     }
@@ -40,11 +43,10 @@ Engine.prototype = {
 
 // create a System for executing everything.
 var System = function () {
-    // 系统名称
-    this.name = '欧鹏斯玛特系统';
     // 系统运行数据
     this.data = arguments;
 
+    // 初始化系统参数
     this.initialize = function () {
         console.log('Initialize system');
     }
@@ -53,23 +55,39 @@ var System = function () {
     this.enableLauncher = function () {
         this.initialize();
         this.execute();
+        this.doSomething();
         return JSON.stringify(this.data);
     }
 }
 
 System.prototype = Object.create(Engine.prototype);
 System.prototype.constructor = System;
-System.prototype.demo = function () {
-
+// 系统名称
+System.prototype.name = '欧鹏斯玛特系统';
+System.prototype.type = 'system';
+System.prototype.description = 'System Service';
+System.prototype.extra = function () {
+    // 附加函数,用于扩展系统其他功能,选择函数或JSON格式对象操作
+}
+System.prototype.doSomething = function () {
+    console.log("This is a " + this.description + " for us to do something.");
 }
 
 // create a Application and run it.
 var Application = {
     name: '应用系统',
     type: 'app',
+    description: 'Application Service',
     run: function () {
-        var SystemInstance = new System();
-        SystemInstance.enableLauncher();
+        var data = {"success": false, "data": [], "code": 100001, "type": "application/json", "message": "System error | 系统错误"};
+        var SystemInstance = new System(data);
+        console.log(SystemInstance.enableLauncher());
+    },
+    extra: function () {
+
+    },
+    doSomething : function () {
+        console.log("This is a " + this.description + " for us to do something.");
     }
 };
 
@@ -77,9 +95,6 @@ var Application = {
 (function () {
     // 系统开始运行时间
     console.time('system');
-
-    // 系统测试.
-    //console.info(System.prototype.doSomething());
 
     // 应用系统运行
     Application.run();
